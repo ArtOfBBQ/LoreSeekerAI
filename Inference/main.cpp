c3f684ff6a061c648ef7813b19b5aa9e90a2a318
Jelle Van den Eynde <40927974+ArtOfBBQ@users.noreply.github.com>
2021-02-04 03:09:57 +0900

    including tensorflow lite headers to predict from c++

---
+++ Inference/main.cpp
@@ -0,0 +1,12 @@
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+}
+
